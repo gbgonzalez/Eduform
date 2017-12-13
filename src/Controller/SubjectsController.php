21ce79cc2cb55e915f724a31e2a6a222f1477246
dmosquerad <dmosquerad@gmail.com>
2017-12-13 02:06:38 +0100

    Update SubjectsController.php

--- src/Controller/SubjectsController.php
+++ src/Controller/SubjectsController.php
@@ -18,7 +18,7 @@ class SubjectsController extends AppController {
         // Allow users to register and logout.
         // You should not add the "login" action to allow list. Doing so would
         // cause problems with normal functioning of AuthComponent.
-        $this->Auth->allow(['add', 'logout']);
+        $this->Auth->allow(['logout']);
     }
 
     public function index()
@@ -95,6 +95,17 @@ class SubjectsController extends AppController {
         
 
         //$this->set('user', $article);
+    }
+
+    public function isAuthorized($user)
+    {
+
+        // Admin can access every action
+        if (($user['role'] === 'Administrador')) {
+            return true;
+        }
+       
+        return false;
     }
 
 }
