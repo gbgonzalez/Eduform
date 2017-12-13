2a927865be9b702a03f71ed8a579798926dda8de
dmosquerad <dmosquerad@gmail.com>
2017-12-13 02:04:57 +0100

    Update EduformController.php

--- src/Controller/EduformController.php
+++ src/Controller/EduformController.php
@@ -16,13 +16,14 @@
             // Allow users to register and logout.
             // You should not add the "login" action to allow list. Doing so would
             // cause problems with normal functioning of AuthComponent.
-            $this->Auth->allow(['add', 'logout']);
+            $this->Auth->allow(['index', 'logout']);
         }
 
     	public function index()
         {
             if ($this->request->is('post')) {
 
+ //DNI
                 if (Validation::numeric($this->request->data['username'])) {
                     $this->Auth->config('authenticate', [
                         'Form' => [
@@ -32,6 +33,20 @@
                     $this->Auth->constructAuthenticate();
                     $this->request->data['dni'] = $this->request->data['username'];
                     unset($this->request->data['username']);
+                }
+ 
+ //EMAIL
+         
+                if (Validation::email($this->request->data['username'])) {
+                    $this->Auth->config('authenticate', [
+                        'Form' => [
+                            'fields' => ['username' => 'email']
+                       ]
+                    ]);
+                    $this->Auth->constructAuthenticate();
+                    $this->request->data['email'] = $this->request->data['username'];
+                    unset($this->request->data['username']);
+                   $this->set('current_user', $this->Auth->user());
                 }
 
                 $user = $this->Auth->identify();
@@ -43,6 +58,7 @@
                     /**/
                     return $this->redirect($this->Auth->redirectUrl());
                 }
+                
 
                 $this->Flash->error(__('Invalid username or password, try again'));
             }
@@ -59,6 +75,15 @@
             return $this->redirect($this->Auth->logout());
         }
 
+        public function isAuthorized($user)
+        {
+            // Admin can access every action
+            if (($user['role'] === 'Administrador' || $user['role'] === 'Alumno' || $user['role'] === 'Gestor de Contenidos')) {
+                return true;
+            }
+            
+            return false;
+        }
 
     }
 
