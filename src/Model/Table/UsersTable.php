9a27ad685b6ec549286da39a4301610a16144435
dmosquerad <dmosquerad@gmail.com>
2018-01-03 01:40:27 +0100

    Add files via upload
    No poder crear usuarios con solo caracteres numericos

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -30,6 +30,10 @@ class UsersTable extends Table
         return $validator
             ->notEmpty('dni', 'A dni is required')
             ->notEmpty('username', 'A username is required')
+            ->add('username', 'validFormat',[
+                'rule' => array('custom', '/([a-zA-Z]+)/i'),
+                'message' => 'Please enter a valid serial number.'
+            ])
             ->notEmpty('password', 'A password is required')
             ->notEmpty('role', 'A role is required')
             ->add('role', 'inList', [
