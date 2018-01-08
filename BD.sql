db5acbffb85efbd5bb153a75e9d9d2c2a4229506
Guillermo Blanco <guillermo@guillermoblanco.es>
2018-01-08 19:49:47 +0100

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -168,7 +168,7 @@ INSERT INTO userscompetences (user_id, competence_id, created)
 INSERT INTO userscompetences (user_id, competence_id, numericnote, created) 
 	VALUES(1,6,'3',NOW());
 INSERT INTO userscompetences (user_id, competence_id, created) 
-	VALUES(2,1,,NOW());
+	VALUES(2,1,NOW());
 INSERT INTO userscompetences (user_id, competence_id, created) 
 	VALUES(2,2,NOW());
 INSERT INTO userscompetences (user_id, competence_id, created) 
